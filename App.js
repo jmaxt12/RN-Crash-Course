4d04bd396ee5d6063f614c965b7b65756e75b647
Jake <jmaxt12@gmail.com>
2020-02-26 09:28:43 -0700

    changed inline styles to use StyleSheet

--- App.js
+++ App.js
@@ -1,12 +1,24 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, StyleSheet } from 'react-native';
 
 const App = () => {
   return ( 
-    <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
-      <Text style={{color: 'darkslateblue', fontSize: 30}}>Hello World</Text>
+    <View style={styles.containier}>
+      <Text style={styles.text}>Hello World</Text>
     </View>
    );
 }
+
+const styles = StyleSheet.create({
+  containier: {
+    flex: 1, 
+    justifyContent: 'center', 
+    alignItems: 'center'
+  },
+  text: {
+    color: 'darkslateblue', 
+    fontSize: 30
+  }
+})
  
 export default App;
