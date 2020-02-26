7832455a4e19f3cf204ac23e67fbe8aaa2a95373
Jake <jmaxt12@gmail.com>
2020-02-26 09:42:07 -0700

    app.js back to beginning for real start

--- App.js
+++ App.js
@@ -1,13 +1,10 @@
 import React from 'react';
-import { View, Text, Image, StyleSheet } from 'react-native';
+import { View, Text, StyleSheet } from 'react-native';
 
 const App = () => {
   return ( 
     <View style={styles.containier}>
-      <Text style={styles.text}>Hello World</Text>
-      <Image 
-        source={{uri: 'https://randomuser.me/api/portraits/men/3.jpg'}} 
-        style={styles.img}/>
+      <Text>Hello World</Text>
     </View>
    );
 }
@@ -15,17 +12,7 @@ const App = () => {
 const styles = StyleSheet.create({
   containier: {
     flex: 1, 
-    justifyContent: 'center', 
-    alignItems: 'center'
   },
-  text: {
-    color: 'darkslateblue', 
-    fontSize: 30
-  },
-  img: {
-    width: 100,
-    height: 100
-  }
 })
  
 export default App;
