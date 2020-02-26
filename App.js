26baca84ba797b90f505558b725e6bb1ace42124
Jake <jmaxt12@gmail.com>
2020-02-26 10:15:06 -0700

    added state

--- App.js
+++ App.js
@@ -1,8 +1,17 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { View, Text, StyleSheet } from 'react-native';
 import Header from './components/Header';
+import { uuid } from 'uuidv4';
 
 const App = () => {
+
+  const [items, setItems] = useState([
+    {id: uuid(), text: 'Milk'},
+    {id: uuid(), text: 'Eggs'},
+    {id: uuid(), text: 'Bread'},
+    {id: uuid(), text: 'Juice'},
+  ])
+
   return ( 
     <View style={styles.containier}>
       <Header title='Shopping List'/>
