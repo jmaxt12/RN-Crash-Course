9f1d3c7087fd91e3f62959319a5b9ded3878f79f
Jake <jmaxt12@gmail.com>
2020-02-26 10:46:30 -0700

    list item styled

--- App.js
+++ App.js
@@ -1,20 +1,25 @@
 import React, { useState } from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import { View, Text, StyleSheet, FlatList } from 'react-native';
 import Header from './components/Header';
-import { uuid } from 'uuidv4';
+import ListItem from './components/ListItem';
+//import { uuid } from 'uuidv4';
 
 const App = () => {
 
   const [items, setItems] = useState([
-    {id: uuid(), text: 'Milk'},
-    {id: uuid(), text: 'Eggs'},
-    {id: uuid(), text: 'Bread'},
-    {id: uuid(), text: 'Juice'},
+    {id: Math.random(), text: 'Milk'},
+    {id: Math.random(), text: 'Eggs'},
+    {id: Math.random(), text: 'Bread'},
+    {id: Math.random(), text: 'Juice'},
   ])
 
   return ( 
     <View style={styles.containier}>
       <Header title='Shopping List'/>
+      <FlatList 
+        data={items}
+        renderItem={({item}) => <ListItem item={item}/>}
+      />
     </View>
    );
 }
